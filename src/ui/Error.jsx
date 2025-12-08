b96a41006b68769159bc16e833e7609ed1755fb4
unknown <ondizputed@gmail.com>
2025-12-08 13:56:51 +0100

    updates error page

--- src/ui/Error.jsx
+++ src/ui/Error.jsx
@@ -1,19 +1,12 @@
 import { useRouteError } from "react-router-dom"
-// import errorImage from "../../public/errorImage.jpg"
-import { motion, AnimatePresence, animate, scale } from "framer-motion"
 
 function Error () {
     const error = useRouteError()
 
     return (
-        // <section className="h-full flex justify-center items-center">
-        //     <div className="py-8 px-10 border border-yellow-500 rounded">
-        //         <h1 className="text-lg sm:text-xl font-bold">{error.data || error.message}</h1>
-        //     </div>
-        // </section>
-        <motion.div initial={{scale: 0, opacity: 0}} animate={{scale: 1, opacity: 1}} className="bg-yellow-500 m-auto p-12 w-30">
-            <p>going through the motions.</p>
-        </motion.div>
+        <div className="bg-yellow-500 p-12 w-full h-screen">
+            <p>something went wrong: <span className="font-bold">{error.data || error.message}</span></p>
+        </div>
     )
 }
 
